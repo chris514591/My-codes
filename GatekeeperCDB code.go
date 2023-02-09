d1a7a6f25f8dba56c3853687c1022bdac8ec7274
Chris de Bruijn <514591@student.fontys.nl>
2023-02-09 14:32:59 +0100

    Changes for cleaner code

--- GatekeeperCDB code.go
+++ GatekeeperCDB code.go
@@ -5,20 +5,22 @@ import (
 	"time"
 )
 
-//V2
-
 func main() {
 	fmt.Println("Welcome to Fonteyn Holidayparks")
+
 	currentTime := time.Now()
 	hour := currentTime.Hour()
 
 	if hour >= 7 && hour < 12 {
 		fmt.Println("Goodmorning!")
-	} else if hour >= 12 && hour < 18 {
-		fmt.Println("Good afternoon!")
-	} else if hour >= 18 && hour < 23 {
-		fmt.Println("Good evening!")
-	} else if hour >= 23 && hour < 7 {
-		fmt.Println("The parking lot is closed. Sorry for the inconvenience.")
+	} 
+	else if hour >= 12 && hour < 18 {
+	fmt.Println("Good afternoon!")
+	} 
+	else if hour >= 18 && hour < 23 {
+	fmt.Println("Good evening!")
+	} 
+	else if hour >= 23 && hour < 7 {
+	fmt.Println("The parking lot is closed. Sorry for the inconvenience.")
 	}
 }
