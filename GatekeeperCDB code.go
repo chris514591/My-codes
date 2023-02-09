e013aa8e87be118f4a51704db55bdabeca6eaecf
Chris de Bruijn <514591@student.fontys.nl>
2023-02-09 14:06:43 +0100

    first commit

---
+++ GatekeeperCDB code.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func morgen() {
+
+}
+
+func middag() {
+
+}
+
+func avond() {
+
+}
+
+func gesloten() {
+
+}
+
+//V2
+
+func main() {
+	fmt.Println("Welkom bij Fonteyn vakantieparken!")
+}
