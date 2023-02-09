de082cf765cfac5402141d0bc1d9e02019ac674e
Chris de Bruijn <514591@student.fontys.nl>
2023-02-09 14:22:48 +0100

    GatekeeperV2

--- GatekeeperCDB code.go
+++ GatekeeperCDB code.go
@@ -1,25 +1,24 @@
 package main
 
-import "fmt"
-
-func morgen() {
-
-}
-
-func middag() {
-
-}
-
-func avond() {
-
-}
-
-func gesloten() {
-
-}
+import (
+	"fmt"
+	"time"
+)
 
 //V2
 
 func main() {
 	fmt.Println("Welkom bij Fonteyn vakantieparken!!!")
+	currentTime := time.Now()
+	hour := currentTime.Hour()
+
+	if hour >= 7 && hour < 12 {
+		fmt.Println("Goedemorgen!")
+	} else if hour >= 12 && hour < 18 {
+		fmt.Println("Goedemiddag!")
+	} else if hour >= 18 && hour < 23 {
+		fmt.Println("Goedenavond!")
+	} else if hour >= 23 && hour < 7 {
+		fmt.Println("Sorry, de parkeerplaats is 's nachts gesloten!")
+	}
 }
