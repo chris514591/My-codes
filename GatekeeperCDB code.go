2bcb78f501ee7bb764edfc16aee1c77c473e0183
Chris de Bruijn <514591@student.fontys.nl>
2023-02-09 14:48:02 +0100

    Function for welcomeOrNot

--- GatekeeperCDB code.go
+++ GatekeeperCDB code.go
@@ -5,22 +5,24 @@ import (
 	"time"
 )
 
-func main() {
-	fmt.Println("Welcome to Fonteyn Holidayparks")
+func welcomeOrNot() {
 
 	currentTime := time.Now()
 	hour := currentTime.Hour()
 
 	if hour >= 7 && hour < 12 {
 		fmt.Println("Goodmorning!")
-	} 
-	else if hour >= 12 && hour < 18 {
-	fmt.Println("Good afternoon!")
-	} 
-	else if hour >= 18 && hour < 23 {
-	fmt.Println("Good evening!")
-	} 
-	else if hour >= 23 && hour < 7 {
-	fmt.Println("The parking lot is closed. Sorry for the inconvenience.")
+	} else if hour >= 12 && hour < 18 {
+		fmt.Println("Good afternoon!")
+	} else if hour >= 18 && hour < 23 {
+		fmt.Println("Good evening!")
+	} else if hour >= 23 && hour < 7 {
+		fmt.Println("The parking lot is closed. Sorry for the inconvenience.")
 	}
+}
+
+func main() {
+	fmt.Println("Welcome to Fonteyn Holidayparks!")
+
+	welcomeOrNot()
 }
