c4d49d8fce5fa89e62a666ef7f29d9bbf9fcc54b
Chris de Bruijn <514591@student.fontys.nl>
2023-02-16 10:29:39 +0100

    GatekeeperV3 added commentary

--- GatekeeperCDB code.go
+++ GatekeeperCDB code.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"os"
 	"time"
+	// Needed to use certain things in your code, may be added automatically. Assume you need to add them yourself
 )
 
 func welcomeOrNot() {
 
 	currentTime := time.Now()
 	hour := currentTime.Hour()
+	// Translate the current time to currentTime and than to "hour"
 
 	if hour >= 7 && hour < 12 {
 		fmt.Println("Goodmorning!")
@@ -21,20 +23,23 @@ func welcomeOrNot() {
 	} else if hour >= 23 && hour < 7 {
 		fmt.Println("The parking lot is closed. Sorry for the inconvenience.")
 	}
+	// Seperated function to keep de code more clean (My opinion)
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	// Prepare to read terminal
 
 	fmt.Println("Welcome to Fonteyn Holidayparks!")
 	fmt.Println("Please input your license plate number to gain access to the parking lot.")
 
 	scanner.Scan()
-	// convert CRLF to LF
 	text := scanner.Text()
+	// Scan input and save in "text"
 
 	if text == "test" {
 		welcomeOrNot()
+		// Use function if input matches license plate number
 	} else {
 		fmt.Println("License plate number not found, try again.")
 	}
