62298d82df2241a024a9080792e2a923a341ac92
Chris de Bruijn <xevichris@gmail.com>
2023-03-09 14:54:20 +0100

    Setup for start code

--- AlarmError/alarmError.go
+++ AlarmError/alarmError.go
@@ -14,21 +14,18 @@ type Data struct {
 }
 
 func main() {
-	logFile1, err2 := os.OpenFile("alarmErrorLogs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	if err2 != nil {
-		log.Fatal(err2)
+	logFile1, err1 := os.OpenFile("alarmErrorLogs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err1 != nil {
+		os.Exit(1)
 	}
-
 	log.SetOutput(logFile1)
-
 	log.Println("Alarm error program initiated.")
 
-	jsonContent, err1 := ioutil.ReadFile("./alarmConfig.json")
-	if err1 != nil {
-		log.Fatal(err1)
+	jsonContent, err2 := ioutil.ReadFile("./alarmConfig.json")
+	if err2 != nil {
+		log.Fatal(err2)
 		log.SetOutput(logFile1)
 		log.Println("Could not open alarmConfig.json")
 	}
-
 	fmt.Println(jsonContent)
 }
